892ceb7df1d41713e5328435d0ed9fccb7feed91
alexandru avirvarei <alexandru.avi@gmail.com>
2026-01-14 09:51:19 +0200

    added script to add data to postgresql

--- product_web_scrapping.py
+++ product_web_scrapping.py
@@ -1,8 +1,11 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import pandas as pd
 from datetime import datetime
 import re
 import time
+from sqlalchemy import create_engine
+
 
 BASE_URL = "https://www.emag.ro/telefoane-mobile"
 MAX_PAGES = 1
@@ -45,7 +48,6 @@ def scrape_emag_phones():
 
                 results.append({
                     "name": name,
-                    "brand": name.split(),
                     "price_ron": price,
                     "url": link,
                     "scrape_date": scrape_date,
@@ -60,8 +62,30 @@ def scrape_emag_phones():
 
 products = scrape_emag_phones()
 len(products)
-type(products)
-print(products)
 
-for product in range(len(products)):
-    print(products[product]) 
+df = pd.DataFrame(products)
+df.head()
+
+filename = f"emag_phones_{datetime.now().date()}.csv"
+df.to_csv(filename, index=False)
+
+
+
+DB_USER = "postgres"
+DB_PASSWORD = "Acadele12"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "retail"
+
+engine = create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+df.to_sql(
+    name="retail_information",
+    con=engine,
+    if_exists="append",   # important for history
+    index=False
+)
+
+pd.read_sql("SELECT * FROM retail.public.retail_information ORDER BY scrape_date DESC LIMIT 5", engine)
